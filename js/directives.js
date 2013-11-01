4c8a58c1f2adc340d94cd309facc4fe43bfd831b
guoqing2013 <guoqing2013@163.com>
2013-11-01 15:39:32 +0800

    加footer

--- js/directives.js
+++ js/directives.js
@@ -20,7 +20,7 @@ app.directive("appfooter", function($route) {
             '<ul class="footerul">'+
             '<li><a href="#!/" data-icon="home" data-theme="a" class="nav nav1">首页</a></li>'+
             '<li><a href="#!/filterlv1/%7B%22search_type%22:%22good%22,%22lv%22:%221%22%7D" data-icon="grid" data-theme="a" class="nav nav2">分类浏览</a></li>'+
-            '<li><a href="#!/buy" data-icon="star" data-theme="a" class="nav nav3">购物车</a></li>'+
+            '<li><a href="#!/buy" data-icon="shopping-cart" data-theme="a" class="nav nav3">购物车</a></li>'+
             '<li><a href="#!/account" data-icon="gear" data-theme="a" class="nav nav4">会员中心</a></li>'+
             '</ul>'+
             '</div>'+
